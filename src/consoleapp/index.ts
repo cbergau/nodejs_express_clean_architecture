aa87d477b14c46ae418c80326e71b63bafb86857
christianbergau <cbergau86@gmail.com>
2021-11-13 11:48:21 +0100

    parameter validation with a request object and validation and presenting validation errors

--- src/consoleapp/index.ts
+++ src/consoleapp/index.ts
@@ -1,9 +1,11 @@
 import GreetUseCase from "../usecase/greet/GreetUseCase";
 import GreetRequest from "../usecase/greet/GreetRequest";
 import GreetConsoleJsonPresenter from "./presenter/GreetConsoleJsonPresenter";
+import GreetRequestValidator from "../usecase/greet/GreetRequestValidator";
 
 const presenter = new GreetConsoleJsonPresenter()
-const usecase = new GreetUseCase(presenter)
+const validator = new GreetRequestValidator()
+const usecase = new GreetUseCase(presenter, validator)
 const request = new GreetRequest("Christian Bergau")
 
 usecase.execute(request)
