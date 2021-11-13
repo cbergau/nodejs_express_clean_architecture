6654a83d5fcd3549fb85e3f208be1e6995af630c
christianbergau <cbergau86@gmail.com>
2021-11-13 11:55:07 +0100

    add missing test

--- src/consoleapp/presenter/GreetConsoleJsonPresenter.spec.ts
+++ src/consoleapp/presenter/GreetConsoleJsonPresenter.spec.ts
@@ -2,15 +2,25 @@ import GreetConsoleJsonPresenter from "./GreetConsoleJsonPresenter";
 import {expect} from '@jest/globals';
 
 describe("Present Greeting via console", () => {
-    describe("presents normal name", () => {
-        it("should present name as json", () => {
-            console.log = jest.fn();
-            const presenter: GreetConsoleJsonPresenter = new GreetConsoleJsonPresenter()
-            const message = "Hello, Christian Bergau";
+    it("should present name as json", () => {
+        console.log = jest.fn();
+        const presenter: GreetConsoleJsonPresenter = new GreetConsoleJsonPresenter()
+        const message = "Hello, Christian Bergau";
 
-            presenter.present(message)
+        presenter.present(message)
 
-            expect(console.log).toHaveBeenCalledWith(JSON.stringify({message: message}));
-        })
+        expect(console.log).toHaveBeenCalledWith(JSON.stringify({message: message}));
+    })
+
+    it("should present validation errors as json", () => {
+        console.error = jest.fn();
+        const presenter: GreetConsoleJsonPresenter = new GreetConsoleJsonPresenter()
+
+        const errors = new Map<string, Array<string>>()
+        errors.set("name", ["some error"])
+
+        presenter.presentValidationErrors(errors)
+
+        expect(console.error).toHaveBeenCalledWith(`[["name",["some error"]]]`);
     })
 })
