809a0805285c5fa4da06c9648443a41f7c12d7bf
christianbergau <cbergau86@gmail.com>
2021-11-16 16:43:01 +0100

    add test for successful use case execution

--- src/usecase/greet/GreetUseCase.spec.ts
+++ src/usecase/greet/GreetUseCase.spec.ts
@@ -15,4 +15,16 @@ describe("Greet UseCase", () => {
         expect(presenter.errors.size).toBe(1)
         expect(presenter.errors.get("name")).toEqual(["length need to be between 2 and 40"])
     })
+
+    it("should present message if request is valid", () => {
+        const request = new GreetRequest("Christian")
+        const validator = new GreetRequestValidator()
+        const presenter = new GreetPresenterSpy()
+        const usecase = new GreetUseCase(presenter, validator)
+
+        usecase.execute(request)
+
+        expect(presenter.errors.size).toBe(0)
+        expect(presenter.message).toEqual("Hello, Christian")
+    })
 })
