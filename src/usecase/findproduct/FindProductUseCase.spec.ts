055351a3fac16f290c6598b31ca0599c19239006
christianbergau <cbergau86@gmail.com>
2021-12-29 17:16:29 +0100

    finally found a way to mock interfaces with async functions

--- src/usecase/findproduct/FindProductUseCase.spec.ts
+++ src/usecase/findproduct/FindProductUseCase.spec.ts
@@ -1,19 +1,28 @@
 import FindProductUseCase from "./FindProductUseCase";
 import FindProductPresenter from "./FindProductPresenter";
-import ProductRepositoryMock from "../../repository/ProductRepositoryMock";
+import ProductRepository from "../../repository/ProductRepository";
+import Product from "../../entity/Product";
 
 test("Should present found product", async () => {
+    const product = new Product(2, "Clean Architecture")
+
     let presenter: FindProductPresenter = {
         present: jest.fn()
     }
 
-    const repository = new ProductRepositoryMock()
+    let repository: ProductRepository = {
+        findById: jest.fn(async () => {
+            return product
+        }),
+        create: jest.fn()
+    }
+
     const usecase = new FindProductUseCase(presenter, repository)
 
-    await usecase.execute(1)
+    await usecase.execute(2)
 
-    //expect(repository.findById).toHaveBeenCalledWith(1)
-    //expect(repository.findById).toHaveBeenCalledTimes(1)
-    expect(presenter.present).toHaveBeenCalledWith(repository.mockId, repository.mockName)
+    expect(repository.findById).toHaveBeenCalledWith(2)
+    expect(repository.findById).toHaveBeenCalledTimes(1)
+    expect(presenter.present).toHaveBeenCalledWith(2, "Clean Architecture")
     expect(presenter.present).toHaveBeenCalledTimes(1)
 })
