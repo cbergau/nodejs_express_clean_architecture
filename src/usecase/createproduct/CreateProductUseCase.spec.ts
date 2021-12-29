fffba26c6e9d9c935de4a1081a9a4862763285b8
christianbergau <cbergau86@gmail.com>
2021-12-29 17:31:18 +0100

    jest mocking instead of impl

--- src/usecase/createproduct/CreateProductUseCase.spec.ts
+++ src/usecase/createproduct/CreateProductUseCase.spec.ts
@@ -1,17 +1,25 @@
 import CreateProductUseCase from "./CreateProductUseCase";
 import CreateProductPresenter from "./CreateProductPresenter";
-import ProductRepositoryMock from "../../repository/ProductRepositoryMock";
+import ProductRepository from "../../repository/ProductRepository";
+import Product from "../../entity/Product";
 
 test("Should present created product", async () => {
-    let presenter: CreateProductPresenter = {
+    const presenter: CreateProductPresenter = {
         present: jest.fn()
     }
 
-    const repository = new ProductRepositoryMock()
+    const repository: ProductRepository = {
+        findById: jest.fn(),
+        create: jest.fn(async (product: Product) => {
+            product.id = 5
+            return Promise.resolve(product)
+        })
+    }
     const usecase = new CreateProductUseCase(presenter, repository)
 
     await usecase.execute("test")
 
-    expect(presenter.present).toHaveBeenCalledWith(repository.mockId, "test")
+    expect(presenter.present).toHaveBeenCalledWith(5, "test")
     expect(presenter.present).toHaveBeenCalledTimes(1)
+    expect(repository.create).toHaveBeenCalledTimes(1)
 })
