f937a32cbcc84e3a3cd6f390e71a90d379f78d42
christianbergau <cbergau86@gmail.com>
2021-12-30 11:34:54 +0100

    use env for credentials

--- src/repository/PostgresProductRepository.ts
+++ src/repository/PostgresProductRepository.ts
@@ -4,11 +4,11 @@ import ProductRepository from "./ProductRepository";
 
 const Pool = require('pg').Pool
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    database: 'postgres',
-    password: 'postgres',
-    port: 5438
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
 })
 
 export default class PostgresProductRepository implements ProductRepository {
